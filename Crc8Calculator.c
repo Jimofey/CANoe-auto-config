1b69b53cc6db2ba0a22795d27b785da46857dec6
Jimofey <lijin_lyu@foxmail.com>
2022-07-01 12:05:08 +0800

    modify sizeof()

--- Crc8Calculator.c
+++ Crc8Calculator.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
-int Crc_CalculateCRC8Runtime(unsigned int Crc_Length, unsigned int Crc_Data[]){
-    int Crc_DataAddr = 0;
-    int Crc_LoopCounter = 0;
-    int XOR_Value = 0xFF;
-    int Crc_poly = 0x1D;
-    unsigned int init_CRC_Value = 0xFF;
-    unsigned int Crc_Value;
+int Crc_CalculateCRC8Runtime(short Crc_Length, short Crc_Data[])
+{
+    short Crc_DataAddr = 0;
+    short Crc_LoopCounter = 0;
+    short XOR_Value = 0xFF;
+    short Crc_poly = 0x1D;
+    short init_CRC_Value = 0xFF;
+    short Crc_Value;
 
     init_CRC_Value = 0xFF;
     Crc_Value = init_CRC_Value;
@@ -20,7 +21,8 @@ int Crc_CalculateCRC8Runtime(unsigned int Crc_Length, unsigned int Crc_Data[]){
             if ((Crc_Value & 0x80u) > 0)
             {
                 Crc_Value = ((Crc_Value << 1)) ^ Crc_poly;
-            }else
+            }
+            else
             {
                 Crc_Value = (Crc_Value << 1);
             }
@@ -30,10 +32,13 @@ int Crc_CalculateCRC8Runtime(unsigned int Crc_Length, unsigned int Crc_Data[]){
     return Crc_Value;
 }
 
-int main(){
-    unsigned int crcRaw[] = {0x70, 0xA0}, crcValue;
-    crcValue = Crc_CalculateCRC8Runtime(sizeof(crcRaw), crcRaw);
-    printf("0x%x", crcValue); // 0x34
+int main(void)
+{
+    short crcRaw[] = {0x70, 0x00, 0x0F, 0xA0, 0xFA, 0x0A, 0x0C}; // 0xD8
+    short crcValue;
+    crcValue = Crc_CalculateCRC8Runtime(sizeof(crcRaw) / sizeof(crcRaw[0]), crcRaw);
+    // sizeof(crcRaw)： 数组总长度的总字节数 / sizeof(crcRaw[0])： 数组单个元素的字节长度
+    printf("0x%x", crcValue & 0xFF); // 取数值后2位
 
     return 0;
 }
